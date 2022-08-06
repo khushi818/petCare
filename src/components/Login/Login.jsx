b49a218872aaf2444231137057155d15937389af
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-06 11:14:57 +0530

    Adding Backend API Integration with Login and Signup Form and Adding a Loader

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,62 @@
 import { useState } from 'react'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AiOutlineGooglePlus, AiOutlineGithub, AiOutlineTwitter } from 'react-icons/ai'
+import { useEffect } from 'react';
 
 function Login() {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const submitForm = async e => {
+        e.preventDefault();
+        // Displaying a Loader
+        setIsLoading(true);
+
+        const response = await fetch('http://localhost:5000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        const jsonResponse = await response.json();
+        // Removing the Loader
+        setIsLoading(false);
+
+        if (!jsonResponse.errors && !jsonResponse.error) {
+            // Storing the authtoken in the localStorage
+            localStorage.setItem('auth-token', jsonResponse.authtoken);
+            // Redirecting
+            navigate('/');
+        }
+    }
+
+    useEffect(() => {
+      if (localStorage.getItem('auth-token')) {
+        navigate('/');
+      }
+      // eslint-disable-next-line
+    }, []);
+    
     return (
         <main>
             <div className="loginPage container">
                 <h3>Login here 👇</h3>
-                <form>
+                <form onSubmit={submitForm}>
                     <div>
                         <label>Username</label>
-                        <input type="text" onChange={e => setUserName(e.target.value)} />
+                        <input type="text" name="username" value={username} onChange={e => setUserName(e.target.value)} />
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" onChange={e => setPassword(e.target.value)} />
+                        <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
                     </div>
                     <div>
-                        <button type="submit">Submit</button>
+                        <button type="submit">{!isLoading?'Submit': <span class="loader"></span>}</button>
                     </div>
                 </form>
                 <div>
@@ -31,9 +67,9 @@ function Login() {
                     </p>
                 </div>
                 <div className='links'>
-                    <a href="#">{<AiOutlineGooglePlus />}</a>
-                    <a href="#">{<AiOutlineGithub />}</a>
-                    <a href="#">{<AiOutlineTwitter />}</a>
+                    <a href="/">{<AiOutlineGooglePlus />}</a>
+                    <a href="/">{<AiOutlineGithub />}</a>
+                    <a href="/">{<AiOutlineTwitter />}</a>
                 </div>
             </div>
         </main>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,35 +1,72 @@
-import react, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { AiOutlineGooglePlus, AiOutlineGithub, AiOutlineTwitter } from 'react-icons/ai'
 import './SignUp.css'
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+    const [email, setEmail] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const submitForm = async e => {
+        e.preventDefault();
+        // Displaying a Loader
+        setIsLoading(true);
+        
+        const response = await fetch('http://localhost:5000/api/auth/createuser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, username, password }),
+        });
+        const jsonResponse = await response.json();
+        // Removing the Loader
+        setIsLoading(false);
+
+        if (!jsonResponse.errors && !jsonResponse.error) {
+            // Storing the authtoken in the localStorage
+            localStorage.setItem('auth-token', jsonResponse.authtoken);
+            // Redirecting
+            navigate('/');
+        }
+    }
+
+    useEffect(() => {
+      if (localStorage.getItem('auth-token')) {
+        // Redirecting
+        navigate('/');
+      }
+      // eslint-disable-next-line
+    }, []);
     return (
         <main>
             <div className="SignUp container">
                 <h3>SignUp here 👇</h3>
-                <form>
+                <form onSubmit={submitForm}>
                     <div>
                         <label>Email</label>
-                        <input type="Email" onChange={e => setPassword(e.target.value)} />
+                        <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
                     </div>
                     <div>
                         <label>Username</label>
-                        <input type="text" onChange={e => setUserName(e.target.value)} />
+                        <input type="text" name="username" value={username} onChange={e => setUserName(e.target.value)} />
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" onChange={e => setPassword(e.target.value)} />
+                        <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
                     </div>
                     <div>
-                        <button type="submit">Submit</button>
+                        <button type="submit">{!isLoading?'Submit': <span class="loader"></span>}</button>
                     </div>
                 </form>
                 <div className='links'>
-                    <a href="#">{<AiOutlineGooglePlus />}</a>
-                    <a href="#">{<AiOutlineGithub />}</a>
-                    <a href="#">{<AiOutlineTwitter />}</a>
+                    <a href="/">{<AiOutlineGooglePlus />}</a>
+                    <a href="/">{<AiOutlineGithub />}</a>
+                    <a href="/">{<AiOutlineTwitter />}</a>
                 </div>
             </div>
         </main>
