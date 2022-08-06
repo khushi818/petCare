3161dd6d931b2076a9a8fcdcec8a34a374b58035
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-06 21:36:25 +0530

    Fetching all the blogs from the db and showing them in the UI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,40 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './Home.css'
 const Home = () => {
     const [postLists, setPostList] = useState([]);
+
+    useEffect(() => {
+      const fetchAllBlogs = async () => {
+        const response = await fetch('http://localhost:5000/api/blog/getblogs', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const jsonResponse = await response.json();
+        setPostList(jsonResponse);
+      }
+      fetchAllBlogs();
+    }, [])
+    
     return (
         <article>
-            <div className="post">
-                <div>
-                    <div className="title">
-                        <h3>title</h3>
+            {postLists.map((post) => {
+                return (
+                    <div key={post._id} className="post">
+                        <div>
+                            <div className="title">
+                                <h3>{post.title}</h3>
+                            </div>
+                            <div className="postText">
+                                {post.description}
+                            </div>
+                            <h4>@{post.username}</h4>
+                        </div>
                     </div>
-                    <div className="postText">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Voluptatem, qui quis corrupti tempore dicta vero quo odio harum quas maiores
-                        laudantium quam sunt reiciendis ducimus natus, officia quos aliquid quasi pariatur
-                        aspernatur accusantium? Officiis voluptates repellat aspernatur ipsum ipsa,
-                        sunt aperiam dicta sit, omnis iste, sint itaque atque consectetur consequuntur.
-                    </div>
-                    <h4>@joy</h4>
-                </div>
-            </div>
+                );
+            })}
         </article>
     )
 }
