40258e19643abee3b6e45e960d35a959d0cd1a59
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-06 21:36:56 +0530

    Connecting db with the UI form to post a new blog

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,8 +1,44 @@
 import React, { useState } from 'react'
 import './CreatePost.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const CreatePost = () => {
     const [title, setTitle] = useState("");
     const [postText, setPostText] = useState("");
+
+    const submitPost = async () => {
+        const token = localStorage.getItem('auth-token');
+
+        const response = await fetch('http://localhost:5000/api/blog/createblog', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': token
+            },
+            body: JSON.stringify({ title, description: postText }),
+        });
+        const jsonResponse = await response.json();
+
+        if (!jsonResponse.errors && !jsonResponse.error) {
+            setPostText('');
+            setTitle('');
+            toast.success('Your Blog Has been successfully Posted!');
+        }
+
+        // If there are some errors
+        else if (jsonResponse.errors) {
+            const allErrors = jsonResponse.errors;
+            // Looping through all the errors and showing them using a toast
+            for (let error of allErrors) {
+                toast.error(error.msg);
+            }
+        }
+
+        else {
+            toast.error("Internal Server Error");
+        }
+    }
     return (
         <main>
             <div className='container post'>
@@ -10,18 +46,21 @@ const CreatePost = () => {
                 <div className="input_value">
                     <label>Title:</label>
                     <input
+                        value={title}
                         placeholder='Title...'
                         onChange={e => setTitle(e.target.value)} />
                 </div>
                 <div className="input_value">
                     <label>Post:</label>
                     <textarea
+                        value={postText}
                         placeholder='Post...'
                         onChange={e => setPostText(e.target.value)}
                     />
                 </div>
-                <button>Post</button>
+                <button onClick={submitPost}>Post</button>
             </div>
+            <ToastContainer toastStyle={{ backgroundColor: "#202d40", color: 'white' }} />
         </main>
     )
 }
